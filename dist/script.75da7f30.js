595f2009a516f5c8a7fb782c4aacb9cbabf72020
Christine Treacy <christine927t@gmail.com>
2021-04-08 16:33:50 -0400

    added font-awesome package, started project pages

--- dist/script.75da7f30.js
+++ dist/script.75da7f30.js
@@ -3365,7 +3365,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52104" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62201" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
